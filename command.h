e644801a718480c67b6bc3517813cb0195149aa6
wrahi001 <wrahi001@student.ucr.edu>
2016-05-10 14:57:10 -0700

    a redo after undo does not change the index

--- command.h
+++ command.h
@@ -18,23 +18,76 @@ class Command {
 };
 
 class OpCommand : public Command {
-	//OpCommand Code Here
+public:
+	OpCommand(double val) {
+		root = new Op(val);
+	}
+	double execute() {
+		return root->evaluate();
+	}
 };
 
 class AddCommand : public Command {
-	//AddCommand Code Here
+	private:
+	Base *rhs;
+	Add *add;
+
+	public:
+	//Add command
+	AddCommand(Command* c, int op) {
+		rhs = new Op(op);
+		add = new Add(c->get_root(),rhs);
+		root = add;
+	}
+	double execute() {
+		return add->evaluate();
+	}
+		
 };
 
 class SubCommand : public Command {
-	//SubCommand Code Here
+	private:
+	Sub *sub;
+	Base *rhs;
+	public:
+	SubCommand(Command* c, int op){
+		rhs = new Op(op);
+		sub = new Sub(c->get_root(),rhs);
+		root = sub;
+	}
+	double execute() {
+		return sub->evaluate();
+	}
 };
 
 class MultCommand : public Command {
 	//MultCommand Code Here
+	public:
+	MultCommand(Command* c, int op) {
+		rhs = new Op(op);
+		mult = new Mult(c->get_root(),rhs);
+		root = mult;	
+	}
+	double execute() {
+		return mult->evaluate();
+	}
+	private:
+	Mult *mult;
+	Base *rhs;
 };
 
 class SqrCommand : public Command {
 	//SqrCommand Code Here
+	public:
+	SqrCommand(Command* c) {
+		sqr = new Sqr(c->get_root());
+		root = sqr;
+	}
+	double execute() {
+		return sqr->evaluate();
+	}
+	private:
+	Sqr *sqr;
 };
 
 #endif //__COMMAND_CLASS__
--- menu.h
+++ menu.h
@@ -10,30 +10,73 @@ class Menu {
 	private:
 		int history_index;
 		vector<Command*> history;
-
+		bool undoFlag;
+		bool redoFlag;
 	public:
 		Menu() {
 			//Base constructor to set up all necessary members
+			history_index = 0;
+			undoFlag = false;
+			redoFlag = false;
 		};
 		void execute() {
 			//Prints the current commands value (based on history_index), if no commands exist
 			//print 0 by default
+			cout << "Goes into execute func" << endl;
+			if (history_index > 0) {
+			cout << history.at(history_index -1)->execute() << endl;
+			}
+			else {
+				cout << "0" << endl;
+			}
 		};
 		bool initialized() {
 			//Return true if the history has been primed with a single op instruciton
 			//This is necessary because that is the base of a calculation
+			if (history.size() == 0) {
+				return false;
+			}
+			else {
+				return true;
+			}
 		};
 		void add_command(Command* cmd) {
 			//Adds a command to history in the appropriate posiiton (based on history_index)
+			cout << "Goes into add command func" << endl;
+			;
+			if (undoFlag) {
+				history.at(history_index) = cmd;
+				undoFlag = false;
+			}
+			else if (redoFlag) {
+				redoFlag = false;	
+				history.push_back(cmd);
+			}
+			else {
+				history.push_back(cmd);
+			}
+			history_index++;
+			
 		};
 		Command* get_command() {
 			//Returns the command in history we are currently referring to (based on history_index)
+			return history.at(history_index - 1);
 		};
 		void undo() {
 			//Moves to the last command in history (if possible)
+			undoFlag = true;
+			if (history_index != 0) {
+				history_index--;
+			}
+
 		};
 		void redo() {
 			//Moves to the next command in history (if possible)
+			redoFlag = true;
+			if (history_index < history.size()) {
+				history_index++;
+			}
+
 		};
 };
 
